92d7ae9f12014a24073701bf35d9b1de137f515b
xreta <jamshedshadan@gmail.com>
2023-03-19 23:09:30 +0530

    commiting once more

--- mainnn.py
+++ mainnn.py
@@ -1,27 +1,14 @@
 import re
 
-# Open the file containing the tweets
 with open('tweets.txt', 'r') as file:
-
-    # Read the tweets into a list
     tweets = file.readlines()
 
-# Open the file containing the racial slurs
 with open('slurs.txt', 'r') as file:
-
-    # Read the racial slurs into a set
     racialSlurs = set(file.read().split())
 
-# to calculate the degree of profanity for each tweet
 def calculate_profanity(tweet):
-
-    # Use regular expressions to split the tweet into words
     words = re.findall(r'\w+', tweet.lower())
-
-    # Count the number of racial slurs in the tweet
     num_racial_slurs = len(racialSlurs.intersection(words))
-
-    # Calculate the degree of profanity for the tweet
     if num_racial_slurs == 0:
         return "Not profane"
     elif num_racial_slurs == 1:
@@ -31,7 +18,17 @@ def calculate_profanity(tweet):
     else:
         return "Highly profane"
 
-# Calculate the degree of profanity for each tweet 
 for tweet in tweets:
     degree_of_profanity = calculate_profanity(tweet)
     print(f"{tweet.strip()} :: {degree_of_profanity}")
+
+#Steps involved:
+## Open the file containing the tweets
+## Read the tweets into a list
+## Open the file containing the racial slurs
+## Read the racial slurs into a set
+## to calculate the degree of profanity for each tweet
+## Use regular expressions to split the tweet into words
+## Count the number of racial slurs in the tweet
+## Calculate the degree of profanity for the tweet
+## Calculate the degree of profanity for each tweet 
